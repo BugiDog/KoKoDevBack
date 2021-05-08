1067a3d9a313b1971b281294c945d39a461e75c6
BugiDog <sancha0809@inbox.ru>
2021-05-08 13:41:36 +0300

    Add google client

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const express = require('express')
+const auth = require('./API/Authorization')
 const port = 3000
 const app = express()
 app.listen(port, () => console.log(`Server started on ${port} port`))
 console.log("Hello")
 
-app.use('/add', () => {
-    console.log("Ебать робит")
-})
+app.use('/authorization', auth)
